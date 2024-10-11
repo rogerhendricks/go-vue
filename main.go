ad3140faa73fb10ec7e6912a55334207507b4cc4
Roger Hendricks <hendricks.r@gmail.com>
2024-10-11 19:14:56 +1100

    reverted to pre session storage

--- main.go
+++ main.go
@@ -14,7 +14,8 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/session"
 	"github.com/gofiber/fiber/v2/utils"
 	"github.com/rogerhendricks/go-vue/controllers"
-	"github.com/rogerhendricks/go-vue/middleware"
+
+	// "github.com/rogerhendricks/go-vue/middleware"
 	"github.com/rogerhendricks/go-vue/models"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -25,78 +26,75 @@ import (
 var distDir embed.FS
 var db *gorm.DB
 
-
 var store = session.New(session.Config{
-    Expiration: 24 * time.Hour, // Set session expiration time
+	Expiration: 24 * time.Hour, // Set session expiration time
 })
 
-
 func main() {
-    var err error
-    db, err = gorm.Open(sqlite.Open("users.db"), &gorm.Config{})
-    if err != nil {
-        log.Fatal("Failed to connect database")
-    }
+	var err error
+	db, err = gorm.Open(sqlite.Open("users.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect database")
+	}
 
-    // Migrate the schema
-    err = db.AutoMigrate(
-        &models.User{}, &models.Device{},
-        &models.Lead{}, &models.Doctor{},
-        &models.Address{} ,&models.Patient{},
-        &models.ImplantedDevice{}, &models.ImplantedLead{},
-        &models.Report{}, &models.Session{})
-    if err != nil {
-            log.Fatalf("Failed to migrate Report: %v", err)
-    }
+	// Migrate the schema
+	err = db.AutoMigrate(
+		&models.User{}, &models.Device{},
+		&models.Lead{}, &models.Doctor{},
+		&models.Address{}, &models.Patient{},
+		&models.ImplantedDevice{}, &models.ImplantedLead{},
+		&models.Report{}, &models.Session{})
+	if err != nil {
+		log.Fatalf("Failed to migrate Report: %v", err)
+	}
 
-    app := fiber.New()
-    app.Use(cors.New(cors.Config{
-        AllowOrigins:     "http://localhost:8000",
-        AllowMethods:     "GET,POST,PUT,DELETE",
-        AllowHeaders:     "Origin,Content-Type,Accept,Authorization",
-        AllowCredentials: true,
-    }))
+	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:8000",
+		AllowMethods:     "GET,POST,PUT,DELETE",
+		AllowHeaders:     "Origin,Content-Type,Accept,Authorization",
+		AllowCredentials: true,
+	}))
 	app.Use(csrf.New(csrf.Config{
-        KeyLookup:      "header:X-CSRF-Token",
-        CookieName:     "csrf_",
-        CookieSameSite: "Strict",
-        Expiration:     1 * time.Hour,
-        KeyGenerator:   utils.UUID,
-        // Session:           session.Store,
-        SessionKey:        "fiber.csrf.token",
-        HandlerContextKey: "fiber.csrf.handler",
-    }))
-
-
-    SetupRoutes(app)
-
-    app.Static("/uploads", "./uploads")
-
-	app.Use("/", filesystem.New(filesystem.Config{
-		Root:   http.FS(distDir),
-		PathPrefix: "frontend/dist",
-		Index:  "index.html",
-		Browse: false,
+		KeyLookup:      "header:X-CSRF-Token",
+		CookieName:     "csrf_",
+		CookieSameSite: "Strict",
+		Expiration:     1 * time.Hour,
+		KeyGenerator:   utils.UUID,
+		// Session:           session.Store,
+		SessionKey:        "fiber.csrf.token",
+		HandlerContextKey: "fiber.csrf.handler",
 	}))
 
-    app.Use(func(c *fiber.Ctx) error {
-        if strings.HasPrefix(c.Path(), "/assets") {
-            return c.Next()
-        }
-        return c.SendFile("frontend/dist/index.html")
-    })
+	SetupRoutes(app)
 
-    log.Fatal(app.Listen(":8000"))
+	app.Static("/uploads", "./uploads")
+
+	app.Use("/", filesystem.New(filesystem.Config{
+		Root:       http.FS(distDir),
+		PathPrefix: "frontend/dist",
+		Index:      "index.html",
+		Browse:     false,
+	}))
+
+	app.Use(func(c *fiber.Ctx) error {
+		if strings.HasPrefix(c.Path(), "/assets") {
+			return c.Next()
+		}
+		return c.SendFile("frontend/dist/index.html")
+	})
+
+	log.Fatal(app.Listen(":8000"))
 }
 
 func SetupRoutes(app *fiber.App) {
-    api := app.Group("/api", middleware.AuthRequired())
-    // users
-    api.Post("/register", controllers.RegisterUser)
-    api.Post("/login", controllers.LoginUser)
-    api.Post("/logout", controllers.LogoutUser)
-    api.Get("/user", controllers.GetUser)
-    api.Put("/user", controllers.UpdateUser)
+	api := app.Group("/api")
+	// users
+	api.Post("/register", controllers.RegisterUser)
+	api.Post("/login", controllers.LoginUser)
+	api.Post("/logout", controllers.LogoutUser)
+	api.Get("/user", controllers.GetUser)
+	api.Put("/user", controllers.UpdateUser)
 	// devices
 	api.Get("/devices", controllers.GetDevices)
 	api.Post("/devices", controllers.CreateDevice)
@@ -115,36 +113,35 @@ func SetupRoutes(app *fiber.App) {
 	api.Get("/doctors/:id", controllers.GetDoctor)
 	api.Put("/doctors/:id", controllers.UpdateDoctor)
 	api.Delete("/doctors/:id", controllers.DeleteDoctor)
-    // Patients
-    api.Get("/patients", controllers.GetPatients)
-    api.Get("/search/patients", controllers.SearchPatients)
-    api.Post("/patients", controllers.CreatePatient)
-    api.Get("/patients/:id", controllers.GetPatient)
-    api.Put("/patients/:id", controllers.UpdatePatient)
-    api.Delete("/patients/:id", controllers.DeletePatient)
-    // Implanted Devices
-    api.Get("/:id/implantedDevices", controllers.GetImplantedDevices)
-    api.Post("/:id/implantedDevices", controllers.CreateImplantedDevice)
-    api.Get("/implantedDevices/:id", controllers.GetImplantedDevice)
-    api.Put("/implantedDevices/:id", controllers.UpdateImplantedDevice)
-    api.Delete("/implantedDevices/:id", controllers.DeleteImplantedDevice)
-    // Implanted Leads
-    api.Get("/:id/implantedLeads", controllers.GetImplantedLeads)
-    api.Post("/:id/implantedLeads", controllers.CreateImplantedLead)
-    api.Get("/implantedLeads/:id", controllers.GetImplantedLead)
-    api.Put("/implantedLeads/:id", controllers.UpdateImplantedLead)
-    api.Delete("/implantedLeads/:id", controllers.DeleteImplantedLead)
+	// Patients
+	api.Get("/patients", controllers.GetPatients)
+	api.Get("/search/patients", controllers.SearchPatients)
+	api.Post("/patients", controllers.CreatePatient)
+	api.Get("/patients/:id", controllers.GetPatient)
+	api.Put("/patients/:id", controllers.UpdatePatient)
+	api.Delete("/patients/:id", controllers.DeletePatient)
+	// Implanted Devices
+	api.Get("/:id/implantedDevices", controllers.GetImplantedDevices)
+	api.Post("/:id/implantedDevices", controllers.CreateImplantedDevice)
+	api.Get("/implantedDevices/:id", controllers.GetImplantedDevice)
+	api.Put("/implantedDevices/:id", controllers.UpdateImplantedDevice)
+	api.Delete("/implantedDevices/:id", controllers.DeleteImplantedDevice)
+	// Implanted Leads
+	api.Get("/:id/implantedLeads", controllers.GetImplantedLeads)
+	api.Post("/:id/implantedLeads", controllers.CreateImplantedLead)
+	api.Get("/implantedLeads/:id", controllers.GetImplantedLead)
+	api.Put("/implantedLeads/:id", controllers.UpdateImplantedLead)
+	api.Delete("/implantedLeads/:id", controllers.DeleteImplantedLead)
 
-    // Reports
-    api.Get("/:id/reports", controllers.GetReports)
-    api.Post("/:id/reports", controllers.CreateReport)
-    api.Get("/reports/:id", controllers.GetReport)
-    api.Put("/reports/:id", controllers.UpdateReport)
-    api.Delete("/reports/:id", controllers.DeleteReport)
+	// Reports
+	api.Get("/:id/reports", controllers.GetReports)
+	api.Post("/:id/reports", controllers.CreateReport)
+	api.Get("/reports/:id", controllers.GetReport)
+	api.Put("/reports/:id", controllers.UpdateReport)
+	api.Delete("/reports/:id", controllers.DeleteReport)
 
-
-    // Lists
-    api.Get("/deviceList", controllers.GetDevicesList)
-    api.Get("/leadList", controllers.GetLeadsList)
-    api.Get("/doctorsList", controllers.GetDoctorsList)
+	// Lists
+	api.Get("/deviceList", controllers.GetDevicesList)
+	api.Get("/leadList", controllers.GetLeadsList)
+	api.Get("/doctorsList", controllers.GetDoctorsList)
 }
